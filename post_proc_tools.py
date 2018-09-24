f148edf7c6d3567d7e57edb3518fc7b07e7afb97
nmaterise <nick.materise@gmail.com>
2018-09-23 22:42:34 -0600

    Added an expectation value plotter to post_proc_tools.py Finished the transmon.py file for dispersive qubit+cavity Need a phase space generator now and readout

--- post_proc_tools.py
+++ post_proc_tools.py
@@ -11,6 +11,17 @@ import qutip as qt
 import datetime
 
 
+def set_axes_fonts(ax, fsize):
+    """
+    Set axes font sizes because it should be abstracted away
+    """
+    
+    for tick in ax.get_xticklabels():
+        tick.set_fontsize(fsize)
+    for tick in ax.get_yticklabels():
+        tick.set_fontsize(fsize)
+
+
 def get_wigner(psi, file_ext=None):
     """
     Compute, return, and save the Wigner function
@@ -50,10 +61,11 @@ def plot_wigner(xvec, W,
     ax.set_title(tstr, fontsize=tsize)
 
     # Set the axis tick labels to a reasonable size
-    for tick in ax.get_xticklabels():
-        tick.set_fontsize(fsize)
-    for tick in ax.get_yticklabels():
-        tick.set_fontsize(fsize)
+    # for tick in ax.get_xticklabels():
+    #     tick.set_fontsize(fsize)
+    # for tick in ax.get_yticklabels():
+    #     tick.set_fontsize(fsize)
+    set_axes_fonts(ax, fsize)
     
     # Set the color bar
     cbar = fig.colorbar(plt1, ax=ax)
@@ -71,7 +83,37 @@ def plot_wigner(xvec, W,
         
 
 
+def plot_expect(tpts, op_avg, op_name='',
+                tscale='ns', file_ext=None,
+                plot_phase=False):
+    """
+    Plot the expectation value of an operator as a function of time
+    """
 
+
+    # Create the figure and axes
+    fig, ax = plt.subplots(1, 1, figsize=(10, 6))
+    fsize = 24; tsize = 26;
+    set_axes_fonts(ax, fsize)
+    ax.plot(tpts, np.abs(op_avg))
+    
+    # Set the axes labels
+    xstr = 'Time (%s)' % tscale
+    ystr = r'$\langle\hat{%s}\rangle$' % op_name
+
+    ax.set_xlabel(xstr, fontsize=fsize)
+    ax.set_ylabel(ystr, fontsize=fsize)
+    plt.tight_layout()
+    
+    # Save the figure to file
+    if file_ext is not None:
+        fig.savefig('wigner_%s.eps' % file_ext, format='eps') 
+        fig.savefig('wigner_%s.png' % file_ext, format='png') 
+    else:
+        tstamp = datetime.datetime.today().strftime('%y%m%d_%H:%M:%S')
+        fig.savefig('wigner_%s.eps' % tstamp, format='eps') 
+        fig.savefig('wigner_%s.png' % tstamp, format='png') 
+    
 
 
 
--- transmon.py
+++ transmon.py
@@ -5,13 +5,15 @@ Dispersive readout of a transmon qubit Hamiltonian class
 
 import qutip as qt
 import numpy as np
+import post_proc_tools as ppt
+
 
 class transmon_disp:
     """
     Implements the cavity-transmon interaction in the dispersive regime
     """
 
-    def __init__(self, alpha, self_kerr, Nq, Nc):
+    def __init__(self, alpha, self_kerr, Nq, Nc, psi0=None):
         """
         Class constructor
         """
@@ -20,13 +22,36 @@ class transmon_disp:
         # self-Kerr, and cross-Kerr (chi)
         self.alpha = alpha; self.self_kerr = self_kerr;
         self.Nq    = Nq;    self.Nc        = Nc;
-        self.chi = np.sqrt(2 * self_kerr * alpha)
+        self.chi   = np.sqrt(2 * self_kerr * alpha)
+        
+        # Initialize the collapse operators as None
+        self.cops  = None
+        self.set_ops()
+        self.set_init_state(psi0)
+        self.set_H()
+
     
     def __del__(self):
         """
         Class destructor
         """
         pass
+
+
+    @staticmethod
+    def get_cy_window_dict(t1, t2, w, beta, A=1, ph=0, dc=0):
+        """
+        Computes the windowed sine function with start and stop
+        times t1, t2, at frequency w and rise time of the window
+        set by beta. The amplitude of the signal is set by A, and
+        the phase and dc offset are ph and dc
+        """
+
+        # Arguments dictionary
+        args = {'w'  : w,  'a'  : beta, 'A'  : A, 't1' : t1,
+                't2' : t2, 'dc' : dc,   'ph' : ph}
+
+        return args
 
     
     def set_ops(self):
@@ -35,13 +60,154 @@ class transmon_disp:
         compute expectation values
         """
 
+        # Set the transmon operators
+        at0 = qt.destroy(self.Nq)
+        self.at = qt.tensor(at0, qt.qeye(self.Nc))
+
+
+        # Set the cavity operators
+        ac0 = qt.destroy(self.Nc)
+        self.ac = qt.tensor(qt.qeye(self.Nq), ac0)
+        
+
 
     def set_H(self):
         """
         Sets the dispersive Hamiltonian for a transmon coupled to a cavity
         in the rotating frame of the transmon and cavity
+
+        *** Note ***
+        This function does not include the arguments for the string-based
+        Cython time-dependent Hamiltonian, Hc in self.H 
+
         """
 
-        self.H = 
+        # Time independent Hamiltonian
+        H0 = -0.5 * self.alpha * self.at**2 \
+             - self.chi * self.ac.dag()*self.ac * self.at.dag()*self.at
+
+        # Time dependent readout Hamiltonian
+        Hc = (self.ac + self.ac.dag()) * (self.at + self.at.dag())
+        Hc_str = 'A*0.5*(tanh((t - t1)/a)-tanh((t - t2)/a)) * sin(w*t-ph) + dc'
+
+        self.H = [H0, [Hc, Hc_str]]
+
+
+    def set_init_state(self, psi0=None):
+        """
+        Sets the initial state of the system, if None set to the ground state of
+        the qubits and the cavity
+        """
+
+        # Set the state psi0
+        psi_gnd = qt.tensor(qt.basis(self.Nq, 0), qt.basis(self.Nc, 0))
+        self.psi0 = psi0 if psi0 is not None else psi_gnd
+
+
+    def set_cops(self, gamma1, kappa):
+        """
+        Set the collapse operators, assuming the system is shot noise limited,
+        e.g. T2 > T1
+        """
+
+        # Use 1/T1 for the transmon and the line width of the cavity
+        self.cops = [np.sqrt(gamma1) * self.at,
+                     np.sqrt(kappa) * self.ac]
+
+
+
+    def run_dynamics(self, tpts, gamma1, kappa, args):
+        """
+        Run the master equation solver and return the results object
+        """
+
+        # Set the collapse operators if they are None
+        if (self.cops is None) and (gamma1 is not None)\
+            and (kappa is not None):
+            self.set_cops(gamma1, kappa)
+
+
+        # Run the dynamics and return the results object
+        psif = qt.mesolve(self.H, self.psi0, tpts, self.cops, [],
+                         options=qt.Options(nsteps=1000), args=args)
+
+        
+        return psif
+
+
+
+    def get_a_expect(self, psif):
+        """
+        Compute the expectation value of the a operator for the cavity
+        """
+
+
+        # Compute the expectation value and return it
+        a_expect = qt.expect(self.ac, psif.states)
+
+        
+        return a_expect
+
+
+
+    def get_n_expect(self, psif):
+        """
+        Compute the expectation value of the number operator for the transmon
+        """
+
+
+        # Compute the expectation value and return it
+        n_expect = qt.expect(self.at.dag()*self.at, psif.states)
+
+        
+        return n_expect
+
+
+def test_transmon():
+    """
+    Testing function for the above class using a simple example
+    of a three level transmon coupled to a cavity with 16 levels
+    """
+
+    # 16 levels in the cavity, 3 in the transmon
+    Nc = 16; Nq = 3;
+
+    # 250 MHz anharmonicity, 50 MHz self-Kerr
+    alpha = 0.250;      self_kerr = 0.05;
+
+    # Set the cavity linewidth and the transmon T1
+    # T1 = 40 us, kappa = 125 kHz
+    T1 = 40e3; gamma1 = 1./T1; kappa = 0.000125;
+    
+    # Set the initial state
+    psi00 = qt.tensor(qt.basis(Nq, 0), qt.basis(Nc, 0))
+    psi01 = qt.tensor(qt.basis(Nq, 1), qt.basis(Nc, 0))
+    psi0 = (psi00 - psi01).unit()
+    
+    # Create an instance of the class
+    my_tmon = transmon_disp(alpha, self_kerr, Nq, Nc, psi0)
+
+    # Set the time of the simulation in ns
+    tpts = np.linspace(0, 1000, 3001)
+
+
+    # Set the drive parameters for the readout
+    t1 = 0.5*tpts.max(); t2 = tpts.max(); w = alpha; beta = 0.01;
+    args = my_tmon.get_cy_window_dict(t1, t2, w, beta) 
+
+    # Run the mesolver
+    res = my_tmon.run_dynamics(tpts, gamma1, kappa, args) 
+    aavg = my_tmon.get_a_expect(res)
+
+
+    ppt.plot_expect(tpts, aavg, 'a')
+
+
+
+if __name__ == '__main__':
+    
+    # Run the test function above
+    test_transmon()
+
 
 
